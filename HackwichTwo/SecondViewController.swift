046258fd8a716b00d6d20b309329d08000b00dff
Keanna <keannakr@hawaii.edu>
2019-01-30 23:12:21 -1000

    Added label (unfinished)

---
+++ HackwichTwo/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  HackwichTwo
+//
+//  Created by Keanna Ramiscal on 1/30/19.
+//  Copyright © 2019 Keanna Ramiscal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
